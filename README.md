56dbebbbd727d121d522d4dfe0849403ba81dcac
Mattia Baldari <meru3m@users.noreply.github.com>
2018-11-29 18:22:02 +0100

    README improved

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
-# Wordlist_Creator
+# Wordlist Creator manpage
 
 Wordlist Creator is a simple tool which purpose is to produce all the combinations
-of words starting from a pattern. For example, running the command as follows:
+of words starting from a pattern. 
 
-$ python3 wordlist_creator.py %ord -d alphabet
+The basic running command is: 
 
-giving as first argument -> %ord (Each % will be substituted with the second arg)
-and as second argument -> alphabet (in order to serially substitute each % with one letter of the alphabet).
+$ python3 wordlist_creator.py %ord 
 
-It will produce the following output:
+If no dictionary is specified (option -d) then the default one is used. Accepted dictionaries are:
+-d 'alphabet' #(default)
+-d 'numbers'
+-d custom dictionary
+
+The script will recognise the placeholder's position (% this is a placeholder) and it will  substitute each of them based on the provided dictionary.
+
+The basic command above will produce the following output:
+
 aord
 bord
 cord
@@ -18,3 +25,10 @@ word
 sord
 ....
 zord
+
+
+Note: to specify a Custom dictionary simply put letter, symbols or numbers in a file, with no space, no carriage return. For example, if I would use just vocals I would to the script input a file containing:
+
+"aeiou" <- Named for example vocals.txt
+
+command: $ python3 wordlist_creator.py %ord -d /path/to/vocals.txt
--- wordlist_creator.py
+++ wordlist_creator.py
@@ -1,23 +1,43 @@
 # %/usr/bin/env python
 
 """
+##################################################################
+#################### Wordlist_Creator manpage ####################
+##################################################################
+
 Wordlist Creator is a simple tool which purpose is to produce all the combinations
 of words starting from a pattern.
 
--d pattern: Valid patterns are 'alphabet', 'numbers'
+The basic running command is:
 
-For example:
+$ python3 wordlist_creator.py %ord
 
-giving as first argument -> alder%%n (Each % will be substituted with the second arg)
-and as second argument -> alphabet (in order to serially substitute each % with one letter of the alphabet).
+If no dictionary is specified (option -d) then the default one is used. Accepted dictionaries are:
+-d 'alphabet' #(default)
+-d 'numbers'
+-d custom dictionary
 
-Which will produce the following output:
-alderaan
-alderabn
-alderacn
-........
-........
-alderzzn
+The script will recognise the placeholder's position (% this is a placeholder) and it will  substitute each of them based on the provided dictionary.
+
+The basic command above will produce the following output:
+
+aord
+bord
+cord
+....
+word
+....
+sord
+....
+zord
+
+
+Note: to specify a Custom dictionary simply put letter, symbols or numbers in a file, with no space, no carriage return. For example, if I would use just vocals I would to the script input a file containing:
+
+"aeiou" <- Named for example vocals.txt
+
+command: $ python3 wordlist_creator.py %ord -d /path/to/vocals.txt
+
 """
 
 from __future__ import print_function
